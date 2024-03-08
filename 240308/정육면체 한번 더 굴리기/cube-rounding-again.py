407e7e13ccd5a3e924c89879a1ee8aba10dd4ce4
SeongJaePark <111725324+SeongjaeP@users.noreply.github.com>
2024-03-08 20:09:56 +0900

    Title: 정육면체 한번 더 굴리기, Time: 224ms, Memory: 26MB, Status: Passed - CodeTree

--- 240308/정육면체 한번 더 굴리기/cube-rounding-again.py
+++ 240308/정육면체 한번 더 굴리기/cube-rounding-again.py
@@ -1,18 +1,15 @@
-# 중간에 못 풀어서 아래 링크 클론코딩했습니다..
-# https://wikidocs.net/216602
-
 from collections import deque
 
 def getInput():
     n, m = map(int, input().split())
-    matrix = [list(map(int, input().split())) for _ in range(n)]
-    return n, m, matrix
+    graph = [list(map(int, input().split())) for _ in range(n)]
+    return n, m, graph
 
-def move (n, matrix, dir, x, y, row, column):
-    direction = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+def move(n, graph, dir, x, y, row, column):
+    direction = [(0,1), (1,0), (0,-1), (-1,0)]  
     nx, ny = x + direction[dir][0], y + direction[dir][1]
-    if nx < 0 or nx == n or ny < 0 or ny == n:
-        dir = (dir + 2) % 4
+    if 0>nx or n==nx or 0>ny or n==ny:  
+        dir = (dir+2)%4
         nx, ny = x + direction[dir][0], y + direction[dir][1]
 
     if dir==0:
@@ -39,28 +36,28 @@ def move (n, matrix, dir, x, y, row, column):
 
     return dir, nx, ny, row, column
 
-
-def score(n, matrix, dir, x, y):
-    direction = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    start = matrix[x][y]
+def score(n, graph, dir, x, y):
+    direction = [(0,1), (1,0), (0,-1), (-1,0)]
+    start = graph[x][y]
     q = deque([(x,y)])
     llist = {(x,y)}
-    cnt = 1
+    count = 1
 
     while q:
         x, y = q.popleft()
+
         for i in range(4):
             nx, ny = x + direction[i][0], y + direction[i][1]
-            if 0 <= nx < n and 0 <= ny < n and matrix[nx][ny] == start and (nx, ny) not in llist:
-                cnt += 1
-                q.append((nx, ny))
-                llist.add((nx, ny))
+            if 0<=nx<n and 0<=ny<n and graph[nx][ny]==start and (nx,ny) not in llist:
+                count+=1
+                q.append((nx,ny))
+                llist.add((nx,ny))
 
-    return start * cnt
+    return start*count
 
 
 def main():
-    n, m, matrix = getInput()
+    n, m, graph = getInput()
     x, y = 0, 0
     dir = 0
     row = deque([1, 3, 6, 4])
@@ -68,8 +65,8 @@ def main():
     answer = 0
 
     for _ in range(m):
-        dir, x, y, row, column = move(n, matrix, dir, x, y, row, column)
-        answer += score(n, matrix, dir, x, y)
+        dir, x, y, row, column = move(n, graph, dir, x, y, row, column)
+        answer += score(n, graph, dir, x, y)
 
     print(answer)
 
