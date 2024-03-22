488ba2b0dbec6e61cfe1978b0638e69b2cf5d429
SeongJaePark <111725324+SeongjaeP@users.noreply.github.com>
2024-03-22 17:14:23 +0900

    Title: 돌아가는 팔각 의자, Time: 185ms, Memory: 25MB, Status: Passed - CodeTree

--- 240322/돌아가는 팔각 의자/rounding-eight-angle.py
+++ 240322/돌아가는 팔각 의자/rounding-eight-angle.py
@@ -1,19 +1,18 @@
-from collections import deque
+from collections import deque   
 
-def left(n, d):
-    if n < 0:
-        return
+def left(gear_index, direction):
+    if gear_index < 0: return
 
-    if gears[n][2] != gears[n+1][6]:
-        left(n-1, -d)
-        gears[n].rotate(d)
+    if gears[gear_index][2] != gears[gear_index+1][6]:
+        left(gear_index-1, -direction)
+        gears[gear_index].rotate(direction)
 
-def right(n, d):
-    if n > 3:
-        return
-    if gears[n-1][2] != gears[n][6]:
-        right(n+1, d)
-        gears[n].rotate(d)
+def right(gear_index, direction):
+    if gear_index > 3: return
+
+    if gears[gear_index-1][2] != gears[gear_index][6]:
+        right(gear_index+1, -direction)
+        gears[gear_index].rotate(direction)
 
 def calculate_score():
     score = 0
@@ -22,15 +21,19 @@ def calculate_score():
             score += 2**i
     return score
 
+# 톱니바퀴 초기화
 gears = [deque(input()) for _ in range(4)]
+
+# 회전 횟수
 k = int(input())
 
+# 회전 실행
 for _ in range(k):
-    n, d = map(int ,input().split())
-    n -= 1
+    gear_index, direction = map(int, input().split())
+    gear_index -= 1  # 인덱스 조정
 
-    left(n-1, -d)
-    right(n+1, -d)
-    gears[n].rotate(d)
+    left(gear_index - 1, -direction)
+    right(gear_index + 1, -direction)
+    gears[gear_index].rotate(direction)
 
 print(calculate_score())
