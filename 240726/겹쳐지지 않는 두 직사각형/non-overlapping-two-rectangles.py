15531b85831b3e6dfcf9d961449bc7126a589a6f
SeongJaePark <111725324+SeongjaeP@users.noreply.github.com>
2024-07-26 12:56:11 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 626ms, Memory: 29MB, Status: Passed - CodeTree

--- 240726/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240726/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -1,19 +1,24 @@
-# 첫번째 직사각형 시작점을 고름
-# 다른 하나의 직사각형 시작점도 고름
-# 직사각형이 안 겹치게 하고
-# 안 겹쳤을 때 합을 더하고 저장
-# 반복하면서 max값 구하기
+# 좌표 정하기
+# 겹치는지 체크
 
 
 
-def rect_sum(x1, y1, x2, y2):
-    return sum([board[i][j] for i in range(x1, x2 + 1) for j in range(y1, y2 + 1)])
+# 최대값 갱신
+
+
+n, m = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
 
 def clear_board():
     for i in range(n):
         for j in range(m):
             visited[i][j] = 0
 
+def draw(x1, y1, x2, y2):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            visited[i][j] += 1
 
 def check_board():
     for i in range(n):
@@ -22,43 +27,36 @@ def check_board():
                 return True
     return False
 
-
-def draw(x1, y1, x2, y2):
-    for i in range(x1, x2 + 1):
-        for j in range(y1, y2 + 1):
-            visited[i][j] += 1
-
-def overlapped(x1, y1, x2, y2, i, j, k, l):
+def overlapped(x1, y1, x2, y2, x3, y3, x4, y4):
     clear_board()
     draw(x1, y1, x2, y2)
-    draw(i, j, k, l)
+    draw(x3, y3, x4, y4)
     return check_board()
 
+def rect_sum(x1, y1, x2, y2):
+    return sum([board[i][j] for i in range(x1, x2 + 1) for j in range(y1, y2 + 1)])
 
 def find_max_sum_with_rect(x1, y1, x2, y2):
-    max_sum = INT_MIN
+    max_sum = -float('INF')
+
     for i in range(n):
         for j in range(m):
             for k in range(i, n):
                 for l in range(j, m):
                     if not overlapped(x1, y1, x2, y2, i, j, k, l):
-                        max_sum = max(max_sum, rect_sum(x1, y1, x2, y2) + rect_sum(i, j, k, l))
+                        max_sum = max(max_sum , rect_sum(x1, y1, x2, y2)+
+                                                rect_sum(i, j, k, l))
 
     return max_sum
 
-
-if __name__ == '__main__':
-    n, m = map(int, input().split())
-    board = [list(map(int,input().split())) for _ in range(n)]
-    visited = [[0] * m for _ in range(n)]
-    INT_MIN = -float('INF')
-
-    max_sum = INT_MIN
-
+def find_max_sum():
+    max_sum = -float('INF')
     for i in range(n):
         for j in range(m):
             for k in range(i, n):
                 for l in range(j, m):
                     max_sum = max(max_sum, find_max_sum_with_rect(i, j, k, l))
+    return max_sum
 
-    print(max_sum)
+ans = find_max_sum()
+print(ans)
