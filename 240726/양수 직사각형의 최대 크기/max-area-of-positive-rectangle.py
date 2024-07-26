7b0d35d6a8d3d0aa759d6f812f7408f57330329f
SeongJaePark <111725324+SeongjaeP@users.noreply.github.com>
2024-07-26 13:44:19 +0900

    Title: 양수 직사각형의 최대 크기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240726/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240726/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -6,8 +6,8 @@ def check_board(x1, y1, x2, y2):
     for i in range(x1, x2 + 1):
         for j in range(y1, y2 + 1):
             if board[i][j] < 0:
-                return False
-    return True
+                return -1
+    return (x2 - x1 + 1) * (y2 - y1 + 1)
 
 def find_max_rec():
     max_area = 0
@@ -15,13 +15,11 @@ def find_max_rec():
         for y1 in range(m):
             for x2 in range(x1, n):
                 for y2 in range(y1, m):
-                    if check_board(x1, y1, x2, y2):
-                        area = (x2 - x1 + 1) * (y2 - y1 + 1)
+                    area = check_board(x1, y1, x2, y2)
+                    if area != -1:
                         max_area = max(max_area, area)
-                    else:
-                        return -1
-    return max_area
-
+                        
+    return max_area if max_area > 0 else -1
 
 max_size = find_max_rec()
 print(max_size)
